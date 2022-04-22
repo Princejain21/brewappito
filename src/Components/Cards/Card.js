9e1138bfc68338def45a46e68f1ea5f6ab532263
prince jain <princejain817111@gmail.com>
2022-04-22 22:31:19 +0530

    card width change

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -3,9 +3,9 @@ import './Card.css'
 
 export default function Card(props) {
   return (
-    <div className='col col-md-4 w-50 my-2 box'>
+    <div className='col col-md-4 col-sm-8  my-2 box'>
     <div className="card "  >
-    <img className="card-img-top w-100 img-fluid" src={props.url?props.url:'https://profootballtalk.nbcsports.com/wp-content/uploads/sites/25/2022/04/GettyImages-1389231641-e1650444900367.jpg'} alt="Card cap" />
+    <img className="card-img-top  img-fluid" src={props.url?props.url:'https://profootballtalk.nbcsports.com/wp-content/uploads/sites/25/2022/04/GettyImages-1389231641-e1650444900367.jpg'} alt="Card cap" />
     <div className="card-body">
       <h5 className="card-title text-white">{props.title}</h5>
       <p className="card-text text-white">{props.desc?props.desc:'A case of attack on an Indian-origin Sikh taxi driver has come to the fore in America.'}</p>
