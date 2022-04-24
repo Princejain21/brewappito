649ef9a7f8e6c19fa9b3ce340002adf6b05df80a
prince jain <princejain817111@gmail.com>
2022-04-25 05:03:10 +0530

    add navbar working

--- src/App.js
+++ src/App.js
@@ -1,41 +1,98 @@
+import React, { useState, useEffect } from 'react'
 import './App.css';
-import React,{useState,useEffect} from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import axios from 'axios'
 import Card from './Components/Cards/Card';
 import Spinner from './Components/spinner/Spinner'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
-  const [spin,setspin]=useState(false);
-  const [data,setdata]=useState([])
-  useEffect(() => { 
+  const [spin, setspin] = useState(false);
+  const [data, setdata] = useState([])
+  useEffect(() => {
     try {
       setspin(false)
-        axios.get(`https://pjnewsdata.herokuapp.com/`)
+      axios.get(`https://pjnewsdata.herokuapp.com/`)
         .then(res => {
-          const persons =res.data;
+          const persons = res.data;
           setspin(true)
           setdata(persons.articles)
         })
     } catch (error) {
-console.log(error)
+      console.log(error)
     }
   }, [])
   return (
     <>
-    <h1 className='text-Roboto'>Live Spaces</h1>
-    <p className='text-monospace'><i className="fa fa-check-circle"></i>All NFTs on Cyber either belong to or were minted by their space creator.</p>
-    <Navbar/>
-    <div className='container'>
-     { !spin &&<Spinner/>}
-    <div className='row w-100 '>
+      <Router>
 
-     {data.length<1 ?null:data.map((elem,index)=>{
-       return <Card key={index} url={elem.urlToImage} author={elem.author===null?"Prince Jain ":elem.author.slice(0,70)} desc={elem.description===null?"hello ":elem.description.slice(0,70)} title={elem.title===null?"There is no title ":elem.title.slice(0,50)} bt1={elem.source.name===null?"Oops! 😑no info... ":elem.source.name.slice(0,18)}  />
-     })}
+        <h1 className='text-Roboto'>Live Spaces</h1>
+        <p className='text-monospace'><i className="fa fa-check-circle"></i>All NFTs on Cyber either belong to or were minted by their space creator.</p>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <div className='container'>
+              {!spin && <Spinner />}
+              <div className='row w-100 '>
+                {data.length < 1 ? null : data.map((elem, index) => {
+                  return <Card key={index} url={elem.urlToImage} author={elem.author === null ? "Prince Jain " : elem.author.slice(0, 70)} desc={elem.description === null ? "hello " : elem.description.slice(0, 70)} title={elem.title === null ? "There is no title " : elem.title.slice(0, 50)} bt1={elem.source.name === null ? "Oops! 😑no info... " : elem.source.name.slice(0, 18)} />
+                })}
+              </div>
+            </div>
+          </Route>
 
-    </div>
-    </div>
+          <Route exact path='/general'>
+          <div className='container'>
+          {!spin && <Spinner />}
+          <div className='row w-100 '>
+        {data.slice(12,21).map((elem,index)=>{
+          return <Card key={index} url={elem.urlToImage} author={elem.author === null ? "Prince Jain " : elem.author.slice(0, 70)} desc={elem.description === null ? "hello " : elem.description.slice(0, 70)} title={elem.title === null ? "There is no title " : elem.title.slice(0, 50)} bt1={elem.source.name === null ? "Oops! 😑no info... " : elem.source.name.slice(0, 18)} />
+        })}
+          
+          </div>
+            </div>
+          
+          </Route>
+          
+          
+          <Route exact path='/sports'>
+          <div className='container'>
+          {!spin && <Spinner />}
+          <div className='row w-100 '>
+        {data.slice(20,35).map((elem,index)=>{
+          return <Card key={index} url={elem.urlToImage} author={elem.author === null ? "Prince Jain " : elem.author.slice(0, 70)} desc={elem.description === null ? "hello " : elem.description.slice(0, 70)} title={elem.title === null ? "There is no title " : elem.title.slice(0, 50)} bt1={elem.source.name === null ? "Oops! 😑no info... " : elem.source.name.slice(0, 18)} />
+        })}
+          
+          </div>
+            </div>
+          </Route>
+          <Route exact path='/science'>
+          <div className='container'>
+          {!spin && <Spinner />}
+          <div className='row w-100 '>
+        {data.slice(37,52).map((elem,index)=>{
+          return <Card key={index} url={elem.urlToImage} author={elem.author === null ? "Prince Jain " : elem.author.slice(0, 70)} desc={elem.description === null ? "hello " : elem.description.slice(0, 70)} title={elem.title === null ? "There is no title " : elem.title.slice(0, 50)} bt1={elem.source.name === null ? "Oops! 😑no info... " : elem.source.name.slice(0, 18)} />
+        })}
+          
+          </div>
+            </div>
+          </Route>
+          <Route exact path='/entertainment'>
+          <div className='container'>
+          {!spin && <Spinner />}
+          <div className='row w-100 '>
+        {data.slice(52,60).map((elem,index)=>{
+          return <Card key={index} url={elem.urlToImage} author={elem.author === null ? "Prince Jain " : elem.author.slice(0, 70)} desc={elem.description === null ? "hello " : elem.description.slice(0, 70)} title={elem.title === null ? "There is no title " : elem.title.slice(0, 50)} bt1={elem.source.name === null ? "Oops! 😑no info... " : elem.source.name.slice(0, 18)} />
+        })}
+          
+          </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './nav.css'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -7,29 +8,24 @@ export default function Navbar() {
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon text-white">&#9776;</span>
     </button>
-    <div className="collapse navbar-collapse justify-content-around " id="navbarNav">
+    <div className="collapse navbar-collapse justify-content-around  " id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="#F">Home <span className="sr-only">(current)</span></a>
+          <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#F">Features</a>
+          <Link className="nav-link" to="/general">General</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#F">Pricing</a>
+          <Link className="nav-link" to="/sports">Sports</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link " href="#F">Disabled</a>
+          <Link className="nav-link " to="/science">Science</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link " href="#F">Features</a>
+          <Link className="nav-link " to="/entertainment">Entertainment</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link " href="#F">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="#F">home</a>
-        </li>
+        
       </ul>
     </div>
   </nav>
